f1a6b10572d24fda1fb21188f323fb82140d18ad
Rodrigo Fernandes <rodrigofs60@gmail.com>
2024-11-12 00:19:17 -0300

    projeto calculadora

---
+++ src/components/Button/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 1px solic #CDCDCD;
+    background-color: #808080;
+    color: #FFFFFF;
+    font-size: 24px;
+    foont-weight: 700;
+    flex: 1;
+
+    &:hover {
+    opacity: 0.6;
+    }
+`
---
+++ src/components/Input/styles.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #000000;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto';
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #000000;
+        border: 0;
+        align: right;
+
+        font-size: 24px;
+        font-family: 'Roboto';
+        padding: 0 10px;
+        color: #FFFFFF;
+    }
+`
